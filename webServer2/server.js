e4bb68b816438dd1f2c69050444179f34ae8f327
Erick93Whiskey <erick@whiskeyindustries.net>
2021-02-16 19:03:56 -0600

    feat: web server static content

--- webServer2/server.js
+++ webServer2/server.js
@@ -1,26 +1,53 @@
+require( 'dotenv' ).config();
 const express = require('express');
+const hbs = require('hbs');
+
+const port = process.env.PORT;
 const app = express();
-const port = 8080;
+
+//HandleBars
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/share' );
+hbs.registerPartials( __dirname + '/views/content' );
  
 //Servir contenido estático
 app.use( express.static( 'public' ) );
 
 app.get( '/', (req, res) => {
-	res.sendFile( __dirname + '/public/index.html' )
+	res.render( 'home', {
+		nombre: 'Erick Alva',
+		titulo: 'Curso de node',
+	});
 });
 
 app.get( '/generic', (req, res) => {
-	res.sendFile( __dirname + '/public/generic.html' )
+	res.render( 'pages', {
+		nombre: 'Erick Alva',
+		titulo: 'Curso de node',
+		classBody: 'subpage',
+		classAlt: 'alt',
+		whichPartial: function() {
+			return "generic";
+	  }
+	});
 });
 
 app.get( '/elements', (req, res) => {
-	res.sendFile( __dirname + '/public/elements.html' )
+	res.render( 'pages', {
+		nombre: 'Erick Alva',
+		titulo: 'Curso de node',
+		classBody: 'subpage',
+		classAlt: 'alt',
+		whichPartial: function() {
+			return "elements";
+	  }
+	});
 });
 
 app.get( '*', (req, res) => {
 	res.sendFile( __dirname + '/public/404.html' )
 });
  
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+app.listen( port, () => {
+	console.log(`Example app listening at http://localhost:${ port }`)
 })
